3f52de1b356941411818f0bd6c5c5d5a4e69227c
rafacm1803 <rafacebamar@gmail.com>
2024-12-13 18:04:01 +0100

    componente mapa

--- Frontend/src/app/pagina2/page.jsx
+++ Frontend/src/app/pagina2/page.jsx
@@ -11,69 +11,24 @@ import OSM from 'ol/source/OSM';
 import Overlay from 'ol/Overlay';
 import { fromLonLat } from 'ol/proj';
 import Navbar from "@/components/navbar";
+import MapaUsuario from "@/components/mapaUsuario";
 
 const USUARIO_BASE_API = process.env.NEXT_PUBLIC_USUARIO_DB_URI;
 
-export default function MapaUsuario() {
+export default function Mapa() {
   const { data: session, status } = useSession();
-  const mapRef = useRef(null);
-  const markersRef = useRef([]);
 
-  useEffect(() => {
-    if (status === 'authenticated') {
-      fetchMarkers();
-    }
-  }, [status]);
-
-  const fetchMarkers = async () => {
-    try {
-      const response = await axios.get(`${USUARIO_BASE_API}/${session.user.email}/marcadores`, {
-        headers: {
-          "Authorization": `Bearer ${session.accessToken}`,
-          "Content-Type": "application/json",
-        },
-      });
-      const markers = response.data;
-      initializeMap(markers);
-    } catch (error) {
-      console.error("Error al obtener los marcadores:", error);
-    }
-  };
-
-  const initializeMap = (markers) => {
-    const map = new Map({
-      target: mapRef.current,
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: fromLonLat([-3.7038, 40.4168]), // Coordenadas iniciales (Madrid)
-        zoom: 2,
-      }),
-    });
-
-    markers.forEach(marker => {
-      const markerElement = document.createElement('div');
-      markerElement.className = 'marker';
-      const overlay = new Overlay({
-        position: fromLonLat([marker.longitud, marker.latitud]),
-        positioning: 'center-center',
-        element: markerElement,
-        stopEvent: false,
-      });
-      map.addOverlay(overlay);
-      markersRef.current.push(overlay);
-    });
-  };
+  if (status === "loading") {
+    return <p>Cargando...</p>;
+  }
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Navbar />
       <main className="flex flex-1 p-4">
-        <div className="map-container flex-1 bg-white rounded-3xl p-8 shadow-lg" style={{ height: '80vh' }}>
-          <div ref={mapRef} style={{ height: '100%', width: '100%' }}></div>
+        <div className="right-section flex-1 bg-white rounded-3xl p-8 shadow-lg flex flex-col justify-center items-center text-center overflow-y-auto max-h-screen">
+          <h1 className="text-2xl font-bold mb-4">Mapa de Marcadores</h1>
+          <MapaUsuario />
         </div>
       </main>
     </div>
